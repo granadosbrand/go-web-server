998ecc1fdd1ac77fb949f086b47ce0e51b249f68
Diego <alejandrogb189@gmail.com>
2024-05-07 09:25:35 -0500

    ch1-8.2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-web-server
 A complete Go web server.
+
+G.DEV 
--- main.go
+++ main.go
@@ -5,33 +5,22 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
-	port := "8080"
 	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
-
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-	corsMux := middlewareCors(mux)
 
-	server := &http.Server{
+	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsMux,
+		Handler: mux,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-	log.Fatal(server.ListenAndServe())
-
-}
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+	log.Fatal(srv.ListenAndServe())
 }
