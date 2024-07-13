a20d79b4cb698601d462438c654b0997a9275454
Diego <alejandrogb189@gmail.com>
2024-07-13 12:16:13 -0500

    chore: Update dependencies and add .env.template and .gitignore entries. S3-DONE

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/granadosbrand/go-web-server
 
 go 1.22.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -3,38 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-type apiConfig struct {
-	fileserverHits int
-}
-
 func main() {
-	const filepathRoot = "."
-	const port = "8080"
+	godotenv.Load(".env")
 
-	apiCfg := apiConfig{
-		fileserverHits: 0,
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
 	}
 
 	mux := http.NewServeMux()
-	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.Handle("/app/*", fsHandler)
 
-	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-	
-	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", handlerErr)
 
-	
-	
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
